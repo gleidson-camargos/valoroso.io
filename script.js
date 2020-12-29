192152b097bae32b0209b286791d49b56497a869
Gleidson Camargos <gleidsoncamargosnet@gmail.com>
2020-12-29 20:03:14 -0300

    primeiro commit[C

---
+++ script.js
@@ -0,0 +1,58 @@
+function mudaphoenixOUT() {
+  let spanjames = document.querySelector(".gago")
+  spanjames.innerHTML = "PHOENIX"
+}
+function mudaphoenixOVER() {
+  let spanjames = document.querySelector(".gago")
+  spanjames.innerHTML = "GAGO"
+}
+
+
+
+function mudabrimstoneOUT() {
+  let spanjames = document.querySelector(".james")
+  spanjames.innerHTML = "BRIMSTONE"
+}
+function mudabrimstoneOVER() {
+  let spanjames = document.querySelector(".james")
+  spanjames.innerHTML = "JAMESLASCENDER"
+}
+
+
+
+function mudaomenOUT() {
+  let spanmatheus = document.querySelector(".matheus")
+  spanmatheus.innerHTML = "OMEN"
+}
+function mudaomenOVER() {
+  let spanmatheus = document.querySelector(".matheus")
+  spanmatheus.innerHTML = "MATPR"
+
+}
+
+function mudarazeOUT() {
+  let spanmatheus = document.querySelector(".gustavo")
+  spanmatheus.innerHTML = "RAZE"
+}
+function mudarazeOVER() {
+  let spanmatheus = document.querySelector(".gustavo")
+  spanmatheus.innerHTML = "GUPR"
+
+}
+
+
+
+function mudasovaOUT() {
+  let spanmatheus = document.querySelector(".ian")
+  spanmatheus.innerHTML = "SOVA"
+}
+function mudasovaOVER() {
+  let spanmatheus = document.querySelector(".ian")
+  spanmatheus.innerHTML = "YODAOREGANO"
+
+}
+
+
+
+
+
